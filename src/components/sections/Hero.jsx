adc72af398cd970ab17f03639859a7630705c438
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-02-21 01:12:17 +0100

    Add the hero file and start

---
+++ src/components/sections/Hero.jsx
@@ -0,0 +1,28 @@
+
+
+export const Hero = () => {
+
+    return (
+        <div className="min-h-screen bg-dark-bg flex items-center mx-auto">
+            <div className="bg-blue-800 w-2xl">
+                <p className="text-3xl text-sage-green font-bold mb-1">
+                    Web Developer Junior
+                </p>
+                <h1 className="text-7xl font-poppins font-bold text-white mb-4">
+                    Nabila Tamajnit
+                </h1>
+                <p className="text-2xl text-gray-400 mt-4">
+                    Passionnée par l'art de transformer une idée en interface fonctionnelle. Le code, terrain d'expression idéal pour ma rigueur et ma créativité.
+                </p>
+
+                <div>
+                    <button className="text-white" >Contactez moi</button>
+                    <button className="text-white" >Télécharger mon CV</button>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
