67b58950989f46e6f2cc862c85d970859d135bc2
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-02-21 01:12:46 +0100

    Import Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 import { Navbar } from "./components/layout/Navbar"
+import { Hero } from "./components/sections/Hero"
 
 function App() {
 
   return (
     <>
-    <Navbar/>
+      <Navbar />
 
-      <div className="min-h-screen bg-dark-bg flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-6xl font-poppins font-bold text-white mb-4">
-          Nabila Tamajnit
-        </h1>
-        <p className="text-xl text-sage-green font-inter">
-          Portfolio avec React et Tailwind
-        </p>
-        <p className="text-sm text-gray-400 mt-4">
-          (Une première pour moi !)
-        </p>
-      </div>
-    </div>
+      <Hero />
     </>
   )
 }
