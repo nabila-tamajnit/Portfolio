177419ee80cb38b4515826431662ed0dcab9ee04
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-02-22 02:24:03 +0100

    Temporarily end the Hero section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,28 +1,52 @@
-
+import { SocialLinks } from "../common/SocialLinks";
 
 export const Hero = () => {
 
     return (
-        <div className="min-h-screen bg-dark-bg flex items-center mx-auto">
-            <div className="bg-blue-800 w-2xl">
-                <p className="text-3xl text-sage-green font-bold mb-1">
-                    Web Developer Junior
-                </p>
-                <h1 className="text-7xl font-poppins font-bold text-white mb-4">
-                    Nabila Tamajnit
-                </h1>
-                <p className="text-2xl text-gray-400 mt-4">
-                    Passionnée par l'art de transformer une idée en interface fonctionnelle. Le code, terrain d'expression idéal pour ma rigueur et ma créativité.
-                </p>
+        <section className="min-h-screen bg-dark-bg flex items-center px-6 lg:px-12" >
 
-                <div>
-                    <button className="text-white" >Contactez moi</button>
-                    <button className="text-white" >Télécharger mon CV</button>
+            <div className="max-w-7xl mx-auto grid lg:grid-cols-2 gap-12 items-center w-full">
+
+                <div className="flex flex-col gap-10">
+
+                    {/* Titre */}
+                    <div className="">
+
+                        <p className="text-3xl text-sage-green font-bold mb-1">
+                            Web Developer Junior
+                        </p>
+
+                        <h1 className="text-7xl font-poppins font-bold text-white mb-4">
+                            Nabila Tamajnit
+                        </h1>
+
+                        <p className="text-2xl text-gray-400 mt-4">
+                            Passionnée par l'art de transformer une idée en interface fonctionnelle. Le code, terrain d'expression idéal pour ma rigueur et ma créativité.
+                        </p>
+
+                    </div>
+
+                    {/* Boutons */}
+                    <div>
+                        <a
+                            href="#"
+                            className="inline-block px-8 py-4 border-2 border-sage-green rounded text-white font-medium hover:bg-sage-green transition-colors"
+                        >
+                            Télécharger mon CV</a>
+                    </div>
+
+                    <SocialLinks />
+
                 </div>
-            </div>
-            <div>
+
+                <div className="max-h-140 aspect-square overflow-hidden bg-[url(/assets/profile_picture/profil-provisoire_1.svg)] bg-center-top bg-no-repeat hover:opacity-50 hover:scale-105 ease-linear " >
+                    {/* <img src="/assets/profil_picture/profil-provisoire.jpeg"
+                        alt="Nabila Tamajnit"
+                        className="w-full h-auto object-cover" /> */}
+                </div>
 
             </div>
-        </div>
+
+        </section>
     )
 }
