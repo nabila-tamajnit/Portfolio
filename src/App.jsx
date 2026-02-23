262915a0edbff563b9fba3971e23a3e926a1a8a2
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-02-24 00:54:29 +0100

    Import Skills and Projects component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Navbar } from "./components/layout/Navbar"
 import { About } from "./components/sections/About"
 import { Hero } from "./components/sections/Hero"
+import { Projects } from "./components/sections/Projects"
+import { Skills } from "./components/sections/Skills"
 
 function App() {
 
@@ -11,6 +13,10 @@ function App() {
       <Hero />
 
       <About />
+
+      <Skills />
+
+      <Projects />
     </>
   )
 }
