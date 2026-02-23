e6c150351fadda5f106ddcdf9191f243480d799f
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-02-23 01:58:20 +0100

    Add and change elements

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -3,43 +3,52 @@ import { SocialLinks } from "../common/SocialLinks";
 export const Hero = () => {
 
     return (
-        <section className="min-h-screen bg-dark-bg flex items-center px-6 lg:px-12" >
+        <section className="min-h-screen bg-dark-bg flex pt-22 px-6 lg:px-12 item" >
 
-            <div className="max-w-7xl mx-auto grid lg:grid-cols-2 gap-12 items-center w-full">
+            <div className="max-w-7xl mx-auto grid lg:grid-cols-2 gap-12 w-full">
 
-                <div className="flex flex-col gap-10">
+                <div className="flex flex-col justify-between gap-10 pt-4 pb-20">
 
                     {/* Titre */}
                     <div className="">
-
-                        <p className="text-3xl text-sage-green font-bold mb-1">
+                        <p className="text-2xl text-sage-green font-bold mb-2">
                             Web Developer Junior
                         </p>
 
-                        <h1 className="text-7xl font-poppins font-bold text-white mb-4">
+                        <h1 className="text-7xl lg:text-8xl font-poppins font-bold text-white mb-6">
                             Nabila Tamajnit
                         </h1>
 
                         <p className="text-2xl text-gray-400 mt-4">
-                            Passionnée par l'art de transformer une idée en interface fonctionnelle. Le code, terrain d'expression idéal pour ma rigueur et ma créativité.
+                            Le développement web n’est plus un rêve lointain, c’est un métier que je façonne chaque jour avec <span className="text-sage-green font-medium">détermination</span>.
+                            J’y exprime ma logique, ma créativité et mon sens du détail.
                         </p>
 
                     </div>
 
                     {/* Boutons */}
-                    <div>
+                    <div className="flex gap-6">
+
                         <a
                             href="#"
-                            className="inline-block px-8 py-4 border-2 border-sage-green rounded text-white font-medium hover:bg-sage-green transition-colors"
+                            className="inline-block px-8 py-4 bg-sage-green rounded text-white font-medium hover:bg-sage-dark transition-colors"
                         >
-                            Télécharger mon CV</a>
+                            Mon LinkThree
+                        </a>
+
+                        <a
+                            href="#"
+                            className="inline-block px-8 py-4 border-2 border-sage-green rounded text-white font-medium hover:bg-sage-white hover:border-sage-white hover:text-black transition-colors"
+                        >
+                            Télécharger mon CV
+                        </a>
                     </div>
 
                     <SocialLinks />
 
                 </div>
 
-                <div className="max-h-140 aspect-square overflow-hidden bg-[url(/assets/profile_picture/profil-provisoire_1.svg)] bg-center-top bg-no-repeat hover:opacity-50 hover:scale-105 ease-linear " >
+                <div className="hidden lg:block h-full overflow-hidden bg-[url(/assets/profile_picture/avatar_profil.png)] bg-top bg-no-repeat bg-cover hover:opacity-90 transition-opacity duration-300" >
                     {/* <img src="/assets/profil_picture/profil-provisoire.jpeg"
                         alt="Nabila Tamajnit"
                         className="w-full h-auto object-cover" /> */}
