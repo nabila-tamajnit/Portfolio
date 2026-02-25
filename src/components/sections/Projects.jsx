d5d54f207717a3e307660e751144ef0ce6def961
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-02-25 01:54:10 +0100

    Add title

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -4,19 +4,27 @@ export const Projects = () => {
 
     return (
 
-        <section className="min-h-screen flex justify-center pt-25">
-            <div>
-                <p className="text-sm lg:text-base text-sage-green font-medium mb-10 flex items-center justify-center gap-2">
-                    <span className=" h-0.5 w-20 bg-gradient-to-r from-sage-green to-white" ></span>
-                    <span className="w-2 h-2 bg-sage-green rounded-full"></span>
-                    03. PROJETS
-                    <span className="w-2 h-2 bg-sage-green rounded-full"></span>
-                    <span className=" h-0.5 w-20 bg-gradient-to-r from-sage-green to-white" ></span>
-                </p>
-                <h2 className=" text:6xl lg:text-7xl font-bold text-green-bg mb-6 text-center" >Mes réalisations</h2>
-                <p className="text-xl text-gray-400 max-w-2xl mx-auto text-center pb-12">
-                    Projets sur lesquels j'ai travaillé durant ma formation
-                </p>
+        <section className="py-32 px-6 lg:px-12">
+            <div className="max-w-7xl mx-auto">
+
+                <div className="text-center mb-20">
+                    <div className="flex items-center justify-center gap-2 mb-8">
+
+                        <div className="h-0.5 bg-gradient-to-r from-sage-green to-sage-green:0 w-20 lg:w-30" ></div>
+                        <div className="w-2 h-2 bg-sage-green rounded-full"></div>
+                        <h4 className="text-sm text-sage-green font-medium tracking-widest">03. PROJETS</h4>
+                        <div className="w-2 h-2 bg-sage-green rounded-full"></div>
+                        <div className="h-0.5 bg-gradient-to-r from-sage-green to-sage-green/0 w-20 lg:w-30" ></div>
+
+                    </div>
+
+                    <h2 className="text-5xl lg:text-6xl font-bold text-green-bg mb-6 leading-tight" >Mes réalisations</h2>
+
+                    <p className="text-xl text-gray-400 max-w-2xl mx-auto">
+                        Projets sur lesquels j'ai travaillé durant ma formation
+                    </p>
+                </div>
+                
             </div>
         </section>
     )
