2a5ca436f73111b27156c59f7c66db1f32c65ee0
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-03-04 00:35:50 +0100

    Modification mobile style

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -28,7 +28,7 @@ export const Hero = () => {
     }, []);
 
     return (
-        <section id="welcome" className="relative overflow-hidden min-h-screen w-full bg-[#080C18] flex justify-start pt-10 px-6  bg-top bg-no-repeat bg-cover" >
+        <section id="welcome" className="relative overflow-hidden min-h-screen w-full bg-bg-main flex justify-start pt-10 px-6  bg-top bg-no-repeat bg-cover" >
 
             <video
                 ref={videoRef}
@@ -38,12 +38,12 @@ export const Hero = () => {
                 playsInline
                 preload="none"
                 poster="/assets/profile_picture/bg_img_hero.png"
-                className=" absolute inset-0 w-full h-full object-cover"
+                className=" absolute inset-0 w-full h-full object-cover object-[73%] lg:object-center"
             >
                 <source src="/assets/profile_picture/bg_video_hero.mp4" type="video/mp4" />
             </video>
 
-            <div className="mx-auto w-full max-w-7xl lg:max-w-full flex justify-center absolute inset-0 bg-black/30 px-6">
+            <div className="mx-auto w-full max-w-7xl lg:max-w-full flex justify-center absolute inset-0 bg-black/50 lg:bg-black/30 px-6">
 
 
                 <div className="relative z-10 flex flex-col pt-25 lg:max-w-250 lg:pr-40 2xl:max-w-290 2xl:pt-35 bg-amber-900/0">
@@ -53,15 +53,15 @@ export const Hero = () => {
                         initial={{ opacity: 0, y: -300 }}
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 2, ease: "lenear" }}>
-                        <p className="text-2xl text-[#5F35D5] font-bold mb-1">
+                        <p className="text-2xl text-purple-accent font-bold mb-1">
                             Web Developer Junior
                         </p>
 
-                        <h1 className="text-7xl lg:text-7xl font-poppins font-medium text-white mb-6">
-                            Hello, je suis <strong className=" text-[#5F35D5] text-8xl">Nabila</strong>
+                        <h1 className="text-5xl lg:text-7xl font-poppins font-medium text-title mb-6">
+                            Hello, je suis <strong className=" text-purple-accent text-6xl lg:text-8xl">Nabila</strong>
                         </h1>
 
-                        <p className="text-2xl text-[#94A3B8]  mb-15">
+                        <p className=" text-lg lg:text-2xl text-white lg:text-text-main  mb-15">
                             Curieuse, déterminée et passionnée par l’univers du web, je prends un réel plaisir à donner vie à des idées à travers le code.
                         </p>
 
@@ -74,7 +74,7 @@ export const Hero = () => {
                             href="#"
                             target="_blanc"
                             rel="noopener noreferrer"
-                            className="inline-block px-8 py-4 border-2 border-[#5F35D5] rounded text-white font-medium hover:bg-[#7C5CFF]
+                            className="inline-block px-3 py-2 lg:px-8 lg:py-4 border-2 bg-purple-hover lg:bg-purple-hover/0 border-purple-hover rounded text-white font-medium hover:bg-purple-hover
                             hover:text-black transition-colors"
                         >
                             Mon LinkThree
@@ -84,7 +84,8 @@ export const Hero = () => {
                             href="/cv/CV_NabilaTamajnit.pdf"
                             target="_blanc"
                             rel="noopener noreferrer"
-                            className="inline-block px-8 py-4 border-2 border-[#5F35D5] rounded text-white font-medium hover:bg-[#7C5CFF]  hover:text-black transition-colors"
+                            className="inline-block px-3 py-2 lg:px-8 lg:py-4 border-2 bg-purple-hover lg:bg-purple-hover/0 border-purple-hover rounded text-white font-medium hover:bg-purple-hover
+                            hover:text-black transition-colors"
                         >
                             <span className="hidden sm:inline-block" >Télécharger mon CV </span><span className="sm:hidden" >Mon CV</span>
                         </a>
