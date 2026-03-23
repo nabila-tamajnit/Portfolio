9b80a2a5e44b8ebe3c3830a142dcd30e2b64a062
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-03-23 03:41:00 +0100

    Add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { Footer } from "./components/layout/footer"
 import { Navbar } from "./components/layout/Navbar"
 import { About } from "./components/sections/About"
+import { Contact } from "./components/sections/Contact"
 import { Hero } from "./components/sections/Hero"
 import { Projects } from "./components/sections/Projects"
 import { Skills } from "./components/sections/Skills"
@@ -17,6 +19,10 @@ function App() {
       <Skills />
 
       <Projects />
+
+      <Contact />
+
+      <Footer />
     </>
   )
 }
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,20 @@
+
+
+
+export const Footer = () => {
+
+    return (
+        <footer className="bg-bg-main flex flex-col items-center">
+
+            <div className="w-full flex items-center justify-center">
+                <div className="h-0.5 bg-gradient-to-l from-[#5F35D5] to-[#5F35D5]/0 w-1/2" ></div>
+                <div className="h-0.5 bg-gradient-to-r from-[#5F35D5] to-[#5F35D5]/0 w-1/2" ></div>
+            </div>
+
+            <div className="text-center text-sm text-title py-10">
+                <p>© 2026 Nabila - Développeuse web en fromation </p>
+                <p>Conçu et développé avec soin</p>
+            </div>
+        </footer>
+    )
+}
