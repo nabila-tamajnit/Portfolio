98307bce46a2ff65667aa0f57cf7039bd165ce11
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-03-24 04:29:27 +0100

    Fix: correct Footer import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Footer } from "./components/layout/footer"
+import { Footer } from "./components/layout/Footer"
 import { Navbar } from "./components/layout/Navbar"
 import { About } from "./components/sections/About"
 import { Contact } from "./components/sections/Contact"
