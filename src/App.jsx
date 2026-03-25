8ad87331eb6ed40943e161f9f684bd7a51b9f93c
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-03-25 17:34:29 +0100

    refresh : links and scroll

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,43 @@ import { Skills } from "./components/sections/Skills"
 
 function App() {
 
-  useEffect(() => {
-    
-    window.scrollTo(0, 0);
-    
-    if ('scrollRestoration' in history) {
-      history.scrollRestoration = 'manual';
+  // ========== REFRESH ==========
+useEffect(() => {
+  let isScrollingToHash = false;
+  const hash = window.location.hash;
+
+  // ----- Via lien -----
+  if (hash) {
+    isScrollingToHash = true;
+    const element = document.querySelector(hash);
+    if (element) {
+      setTimeout(() => {
+        element.scrollIntoView({ behavior: "smooth" });
+        setTimeout(() => { isScrollingToHash = false; }, 800); 
+      }, 100);
     }
-  }, []);
+  }
+
+  // ----- Via scroll -----
+  const sections = document.querySelectorAll("section[id]");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      if (isScrollingToHash) return; 
+
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const id = entry.target.getAttribute("id");
+          window.history.replaceState(null, null, `#${id}`);
+        }
+      });
+    },
+    { threshold: 0.6 }
+  );
+
+  sections.forEach((s) => observer.observe(s));
+  return () => observer.disconnect();
+}, []);
+
 
   return (
     <>
