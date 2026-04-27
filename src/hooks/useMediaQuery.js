bcc312df1b6e3ba6a4b69e7634d29769412963fd
nabila-tamajnit <n.tamajnit.dev@gmail.com>
2026-04-28 00:25:39 +0200

    replace isMobile variable with useMediaQuery hook

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react"
+
+export const useMediaQuery = (query) => {
+    const [matches, setMatches] = useState(
+        () => window.matchMedia(query).matches
+    )
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia(query)
+        const handler = (e) => setMatches(e.matches)
+
+        mediaQuery.addEventListener("change", handler)
+        return () => mediaQuery.removeEventListener("change", handler)
+    }, [query])
+
+    return matches
+}
